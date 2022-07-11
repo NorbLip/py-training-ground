94ffbc8c695268ffa68b4e6c6355e9cc2b09aca7
Norbert <norbert.lipinski@forecast.global>
2022-07-11 15:58:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {'hello': 'world'}
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
